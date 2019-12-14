61c01a91502ae856eba55bc40e1e4551f787c588
sanya-shch <shchipkov.sanya@ukr.net>
2019-12-14 22:54:01 +0200

    added alert context

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useContext} from "react";
+import {AlertContext} from "../context/alert/alertContext";
 
-export const Alert = (alert) => {
-    if (!alert) return null;
+export const Alert = () => {
+    const  {alert, hide} = useContext(AlertContext);
+
+    if (!alert.visible) return null;
 
     return (
         <div className={`alert alert-${alert.type || 'warning'} alert-dismissible fade show`}>
@@ -9,7 +12,7 @@ export const Alert = (alert) => {
 
             {alert.text}
 
-            <button type="button" className="close" aria-label="Close">
+            <button type="button" className="close" onClick={hide} aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import {AlertContext} from '../context/alert/alertContext'
 
 export const Form = () => {
+    const [value, setValue] = useState('');
+    const alert = useContext(AlertContext);
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        if (value.trim()) {
+            alert.show('Note was created', 'success');
+            setValue('');
+        } else {
+            alert.show('Enter a note name')
+        }
+    };
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="form-group">
-                <input type="text" className="form-control" placeholder="Input name of note"/>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Input name of note"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
             </div>
         </form>
     )
